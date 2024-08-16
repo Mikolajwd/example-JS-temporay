9c28d41c518098a2ab3f0715b332213959fd59a0
NikoWD <mikolaj.wd0821@gmail.com>
2024-08-16 21:29:31 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// 1. Pobierz pliki z zasobów.
+// 2. Pobierz w JS paragrafy, przyciski oraz DIV-a square.
+// 3. Stwórz funkcje:
+
+// #1: wyloguj w konsoli napisz "cześć",
+// #2: zmień kolor .square na czerwony
+// #3: zmień kolor square na niebieski ,
+// #4: najpierw dodaj klasę .show do "Paragrafu 1" i usuń klasę .show z "Paragrafu 2", potem zrób na odwrót
+// (przełącznik do światła).
+
+// 4. Funkcje mają być odpalane za pomocą:
+// #1 podwójnego kliknięcia przycisku z burgerem,
+// #2 wjechania myszą na DIV.square,
+// #3 zjechania myszą z DIV.square,
+// #4 kliknięcia w przycisk z pizzą.
+
+const par1 = document.querySelector(".p1");
+const par2 = document.querySelector(".p2.show");
+const btn1 = document.querySelector(".btn.btn1");
+console.log(btn1);
